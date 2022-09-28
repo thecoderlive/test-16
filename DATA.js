d62274dd14b7734ae929df838f2658b4f2538e69
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 14:14:37 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f9e6ba3c-b80c-451a-aa54-ed624a15af0e",
+    "home_feed": [
+        {
+            "id": "1b860996-23a2-47d5-ac35-69329e992f43",
+            "artist_name": "eye",
+            "home_carousel": [
+                {
+                    "id": "c3cfdb08-aad8-40e9-8252-2df98dbdefe2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                },
+                {
+                    "id": "8a726d64-9e82-4c33-93be-33e7869dd20a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "ca4a8399-4cce-48cd-bba4-854f0419675b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "ee9b4d87-d8c4-4f4a-9dc7-352aefd36006",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                },
+                {
+                    "id": "6abd12cf-0229-406d-8141-759c3373d510",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                }
+            ],
+            "art_caption": "parent",
+            "art_description": "Action somebody value approach wonder think both design. Group give front we feel. Interview want vote work."
+        },
+        {
+            "id": "c9181731-91f4-40c8-a662-d8ea9e2cfb75",
+            "artist_name": "study",
+            "home_carousel": [
+                {
+                    "id": "876bc655-7fba-4891-bae8-3ba34e0a0c4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                },
+                {
+                    "id": "faaff57b-2468-4169-86c6-88cd73d6ba20",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "97a445fc-df1a-4448-babc-5c3b2170d080",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "67760514-bb7d-42bb-89d5-ccf9f78b63e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+                },
+                {
+                    "id": "5e3347a2-b69e-4720-9ac8-45796ad6013e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                }
+            ],
+            "art_caption": "police",
+            "art_description": "Resource point million. Issue care against cultural beautiful of."
+        },
+        {
+            "id": "7457243e-7a60-42dc-babe-5545420e5d73",
+            "artist_name": "clearly",
+            "home_carousel": [
+                {
+                    "id": "4aa9ceb1-f29b-4c50-bf69-9de9c13e496b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "59643a20-83e5-42fc-b2b2-3d26f5c6cad8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "b31c71a7-575d-4126-b3ea-6e246cc9e65f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "55917012-3e87-4f66-9ef4-1ce0806cbeb3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "573cbfbc-68c8-4c46-9deb-5db33405e339",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                }
+            ],
+            "art_caption": "return",
+            "art_description": "Various federal next road financial food right course. Power pass than space drug. Arrive computer natural make food get."
+        },
+        {
+            "id": "8ae096de-5f31-49a5-8aba-d39a3964157e",
+            "artist_name": "lay",
+            "home_carousel": [
+                {
+                    "id": "3ea135da-8fae-494c-b96e-db10e2530cba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "5a5a1f2e-f212-4e9e-a493-aeaed1201d9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "d22dfe12-a9c0-4f32-878f-a3c316b7ddaf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png"
+                },
+                {
+                    "id": "a17b679b-5b4f-487f-89d7-ce139a21ff3f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "b7b1f18b-5bc4-4a2b-af9a-2f357b6a7ea4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                }
+            ],
+            "art_caption": "opportunity",
+            "art_description": "Six exactly again true little. Whose candidate new. Open modern good view nearly character under. Boy out place how take media almost authority."
+        },
+        {
+            "id": "9883ebc3-a2e3-4043-9210-008ee2b2cde3",
+            "artist_name": "say",
+            "home_carousel": [
+                {
+                    "id": "21bab7ad-98f0-476d-892e-7d3975deb489",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "836cae42-bf88-43ad-903b-74eac40664ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "42e69399-48a4-423f-93e3-ea2c53526e0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "4987a2ee-f4b1-439d-b995-8a2512a337d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "0d13858f-bf87-44e0-8f66-1c280fa62faf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png"
+                }
+            ],
+            "art_caption": "read",
+            "art_description": "Course different between treatment policy. Half sit center. High admit series whether smile TV everyone camera."
+        }
+    ]
+}
